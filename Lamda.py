9d0418ccc757a8ea428b9cc61dda1f1cc49179eb
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-15 18:05:08 +0530

    15-06

---
+++ Lamda.py
@@ -0,0 +1,12 @@
+# lambda function = function written in 1 line using lambda keyword
+#                   accepts any number of arguments, but only has one expression.
+#                   (think of it as a shortcut)
+#                   (useful if needed for a short period of time, throw-away)
+#
+# lambda parameters:expression
+
+double = lambda x:x*2
+
+multiply = lambda x,y : x*y
+
+print(multiply(5,6))
