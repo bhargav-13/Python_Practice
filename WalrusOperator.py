503669723b59a2dbdc2fbfeba7c7a161e9845f9d
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-15 17:48:36 +0530

    15-06

---
+++ WalrusOperator.py
@@ -0,0 +1,14 @@
+# walrus operator :=
+
+# print(happy := True)  This will print true
+
+foods = list()
+
+# while True:
+#     food = input("What food do you like: ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+while food := input("What food do you like: ") != "quit":
+    foods.append(food)
